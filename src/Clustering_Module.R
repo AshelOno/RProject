d0cfd54d4467babd6087a9fac607257a0dfee0c6
AshelOno <ashelono@gmail.com>
2024-11-23 19:34:01 +0530

    clustering

--- src/Clustering_Module.R
+++ src/Clustering_Module.R
@@ -1,31 +1,25 @@
-# src/Clustering_Module.R
-Clustering_Analysis <- function(data) {
-  library(cluster)
-  
-  prepared_data <- Data_Preparation()
-  
-  # Perform feature engineering
-  engineered_data <- Feature_Engineering(prepared_data)
-  
-  # Select numeric features and scale them
-  clustering_data <- engineered_data %>%
-    select(Price, Duration, price_per_hour) %>%  # Select relevant numeric columns
-    scale()  # Normalize features for distance-based clustering
-  
-  # Perform K-Means clustering
-  set.seed(123)  # For reproducibility
-  kmeans_model <- kmeans(clustering_data, centers = 3, nstart = 10)  # 3 clusters
-  
-  # Add cluster labels to the dataset
-  engineered_data$Cluster <- kmeans_model$cluster
-  
-  # Visualize the clusters
-  fviz_cluster(kmeans_model, data = clustering_data, geom = "point") +
-    ggtitle("K-Means Clustering of Flights")
-  
-  # Return the model and clustered data
-  return(list(model = kmeans_model, clustered_data = engineered_data))
-  
-}
-  
+# Clustering_Module.R
 
+library(cluster)
+library(factoextra)
+
+# Clustering Analysis function
+Clustering_Analysis <- function(data) {
+  # Select relevant columns for clustering
+  clustering_data <- data %>% select(Price, Duration, Days_Left)
+  
+  # Scale the data
+  scaled_data <- scale(clustering_data)
+  
+  # K-means clustering (using 3 clusters as an example)
+  set.seed(123)
+  kmeans_result <- kmeans(scaled_data, centers = 3, nstart = 25)
+  
+  # Add cluster labels to data
+  data$Cluster <- as.factor(kmeans_result$cluster)
+  
+  # Visualize clusters
+  fviz_cluster(kmeans_result, data = scaled_data, geom = "point", main = "Clustering of Flights")
+  
+  return(data)
+}
